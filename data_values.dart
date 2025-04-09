596b4071b76cd0d825c196d8337764d874f1a4ad
Sravya <sravyamendu2382@gmail.com>
2025-04-09 12:09:28 +0530

    Add files via upload

---
+++ data_values.dart
@@ -0,0 +1,146 @@
+class DataValues {
+  // About App
+  static const String appName = 'Sravya Mendu';
+  static const String appVersion = '1.1.2';
+
+  // External links
+  static Uri githubURL = Uri.parse('https://github.com/sravyaMendu23');
+  static Uri linkedinURL = Uri.parse('https://linkedin.com/in/sravya-mendu23');
+  static Uri telegramURL = Uri.parse('https://t.me/sravya_mendu23');
+  static Uri repoURL = Uri.parse('https://github.com/sravyaMendu23/portfolio');
+
+  // Header
+  static const String headerGreetings = 'Hey there! I\'m';
+  static const String headerName = 'Sravya Mendu';
+  static const String headerTitle = 'Student | Developer';
+
+  // Navigation Bar
+  static const String navBarAboutMe = 'About Me';
+  static const String navBarEducation = 'Education';
+  static const String navBarExperience = 'Experience';
+  static const String navBarProjects = 'Projects';
+  static const String navBarContactMe = 'Contact Me';
+
+  // About me
+  static const String aboutMeTitle = 'About Me';
+  static const String aboutMeDescription =
+      'I’m Sravya Mendu, a passionate Flutter developer with experience in creating high-quality mobile apps. This resume highlights my background, education, skills, and work experience. Feel free to reach out for more information.';
+  static const String aboutMeBiographyTitle = 'Biography';
+  static const String aboutMeBiographyDescription =
+      'Hi there! I\'m a computing student and solution developer. I believe in always learning and growing, and I try to apply new knowledge to everything I do.';
+  static const String aboutMeFullNameTitle = 'Full Name';
+  static const String aboutMeFullNameDescription = 'Mendu Sravya';
+  static const String aboutMeNwITitle = 'Name with Initials';
+  static const String aboutMeNwIDescription = 'M. Sravya';
+  static const String aboutMeDobTitle = 'Birthday';
+  static const String aboutMeDobDescription = 'August 23, 2000';
+  static const String aboutMeLanguageTitle = 'Languages';
+  static const String aboutMeLanguageDescription = 'Telugu (Native)\n'
+      'English (Professional)';
+  static const String aboutMeNationalityTitle = 'Nationality';
+  static const String aboutMeNationalityDescription = 'Hindu';
+  static const String aboutMeGenderTitle = 'Gender';
+  static const String aboutMeGenderDescription = 'Female (She/Her)';
+  static const String aboutMeLocationTitle = 'Current Locations';
+  static const String aboutMeLocationDescription = 'Hyderabad';
+
+  static const String aboutMeHobbiesTitle = 'Hobbies';
+  static const String aboutMeHobbiesDescription =
+      'Exploring new ideas and expanding my knowledge.\n'
+      'Engaging in creative arts and crafts\n'
+      'Photography\n'
+      'Jamming to tunes and tuning into podcasts\n';
+  static const String aboutMeGoalTitle = 'Goal';
+  static const String aboutMeGoalDescription =
+      'I aim to keep improving my technical skills and stay updated with industry trends to stay competitive and innovative.';
+  static const String aboutMeStudentTitle = 'Computing Student';
+  static const String aboutMeStudentDescription =
+      'I am a dedicated computing student with a focus on both computer hardware and software development. Through hands-on projects in my master\'s program, I\'ve gained practical experience in applying computing principles to solve real-world problems. Driven by a passion for mastering the fundamentals, I aim to use my knowledge to tackle challenges effectively.';
+  static const String aboutMeDeveloperTitle = 'Software Developer';
+  static const String aboutMeDeveloperDescription =
+      'I use a variety of tools to create innovative solutions aimed at achieving specific goals. My approach includes software and app development with a focus on business objectives. Working with Kotlin and XML on projects has helped streamline processes and improve functionality. I’m committed to using my computing skills to drive meaningful advancements in every project I take on.';
+
+  // Education
+  static const String educationTitle = 'Education';
+  static const String educationDescription =
+      'My educational background is not specific in any particular way and has come from various places. In this section, you can find out about my formal and other sources . So, here are my school, undergraduate institution, and other learning platforms that I\'ve been involving for education.';
+
+  // Education1
+  static const String educationOrg1Title = 'Aurora\'s PG College - Hyderabad';
+  static const String educationOrg1Course1Name = 'Post Graduate';
+  static const String educationOrg1Course1Grade =
+      'Affliated with the Osmania University';
+  static const String educationOrg1Course1Year = '';
+  static const String educationOrg1Course2Name =
+      'Master of Computer Applications(MCA)';
+  static const String educationOrg1Course2Grade = 'Passed with 8.20 CGPA';
+  static const String educationOrg1Course2Year = '2020-2022';
+
+  // Education2
+  static const String educationOrg2Title =
+      'Kanishka Women\'s Degree College - Hanamkonda';
+  static const String educationOrg2Course1Name = 'Under Graduate';
+  static const String educationOrg2Course1Grade =
+      'Affliated with the Kakatiya University';
+  static const String educationOrg2Course1Year =
+      'Maths, Statistics and Computer Science';
+  static const String educationOrg2Course2Name =
+      'Bachelor of Science(Bsc-MSTCS)';
+  static const String educationOrg2Course2Grade = 'Passed with 8.30 CGPA';
+  static const String educationOrg2Course2Year = '2017 - 2020';
+
+  // Education3
+  static const String educationOrg3Title =
+      'Ekashila Junior College - Hanamkonda';
+  static const String educationOrg3Course1Name = 'Intermediate';
+  static const String educationOrg3Course1Grade =
+      'Affliated with the Kakatiya University';
+  static const String educationOrg3Course1Year = 'Maths, Physics and Chemistry';
+  static const String educationOrg3Course2Name = 'Board of Intermediate(MPC)';
+  static const String educationOrg3Course2Grade =
+      'Passed with Aggregate of 8.30';
+  static const String educationOrg3Course2Year = '2015 - 2017';
+
+  // Education4
+  static const String educationOrg4Title = 'Veda Vyasa Vidyalayam - Warangal';
+  static const String educationOrg4Course1Name = 'School';
+  static const String educationOrg4Course1Grade = 'Common Subjects';
+  static const String educationOrg4Course1Year = '';
+  static const String educationOrg4Course2Name =
+      'Board of Scecondary Education';
+  static const String educationOrg4Course2Grade = 'Passed 8.0 CGPA';
+  static const String educationOrg4Course2Year = '2015';
+
+  static const String educationBanner = 'These Are Some Of My Other';
+  static const String educationBannerTitle = 'Online Learning Platforms';
+  static const String educationBannerWeb =
+      'Click here to find out more about my online learning platforms.';
+
+  // Experience
+  static const String experienceTitle = 'Experience';
+  static const String experienceDescription =
+      'During my post-graduation, I delved into new technologies, enhancing my knowledge and skill set. After completing my studies, I began my first full-time position in 2023. Over the past 2 years, I’ve gained valuable experience, which has deepened my understanding and passion for the field. This journey has allowed me to contribute meaningfully to projects, develop practical skills, and adapt to evolving technological landscapes, laying a solid foundation for my career growth.';
+  static const String experienceOrg2Title = 'Toucan Payments';
+  static const String experienceOrg2Role = 'Junior Software Engineer (Flutter)';
+  static const String experienceOrg2Years = 'April 2023';
+  static const String experienceOrg2Vales =
+      'Android, Flutter, Dart, Firebase, Git, REST API and JSON Parsing.';
+
+  //Projects
+  static const String projectsTitle = 'Projects';
+  static const String projectsDescription =
+      'In my projects, I’ve explored various technologies, focusing on the development. I honed my skills for building dynamic and responsive mobile applications, and I gained experience with Flutter for creating cross-platform mobile apps. I have not only developed polished, high-performance applications but also driven innovative solutions that address real-world user needs and set the stage for future advancements in the tech field.';
+
+  // Contact
+  static const String contactTitle = 'Contact';
+  static const String contactDescription =
+      'Hey! Feel free to connect with me through various channels for collaborations, inquiries, or just to say hello. You can find me on social media, messaging apps, and professional networks, or reach out via email. I look forward to engaging with you!';
+  static const String contactBanner = 'Send me an email at';
+  static const String contactEmail = 'sravyamendu2382@gmail.com';
+
+  // Footer
+  static const String builtWith = "Built with Flutter and GitHub Pages.";
+
+  static const String copyright =
+      "Copyright ©2024 Sravya Mendu. All Rights Reserved.";
+}
